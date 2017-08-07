96530c0b6acb3dcda04fc5cf5c7214dba025690a
Jiajiaju <jjiaju@gmail.com>
2017-08-07 23:54:11 +0800

    add some method to Vector2D

--- Classes/utility/Vector2D.hpp
+++ Classes/utility/Vector2D.hpp
@@ -10,7 +10,6 @@
 #define Vector2D_hpp
 
 #include <stdio.h>
-#include <math.h>
 
 class Vector2D{
 private:
@@ -20,7 +19,7 @@ public:
     float x = 0;
     float y = 0;
     
-    Vector2D(): x(0), y(0) {}
+    Vector2D(): x(0.0), y(0.0) {}
     Vector2D(float paramX, float paramY): x(paramX), y(paramY) {}
     Vector2D(const Vector2D& vector2D): x(vector2D.x), y(vector2D.y) {}
     
@@ -45,22 +44,46 @@ public:
         Vector2D newVector2D(this->x / number, this->y / number);
         return newVector2D;
     }
+    bool operator==(const Vector2D& vector2D){
+        return ((x == vector2D.x) && (y == vector2D.y));
+    }
+    bool operator!=(const Vector2D& vector2D){
+        return ((x != vector2D.x) && (y != vector2D.y));
+    }
     
     void normalize(){
-        float distance = sqrtf((this->x * this->x) * (this->y * this->y));
+        float distance = std::sqrt((this->x * this->x) * (this->y * this->y));
         this->x = this->x / distance;
         this->y = this->y / distance;
     }
     Vector2D getNormalize(){
-        float distance = sqrtf((this->x * this->x) * (this->y * this->y));
+        float distance = std::sqrt((this->x * this->x) * (this->y * this->y));
         Vector2D newVector2D(this->x / distance, this->y / distance);
         return newVector2D;
     }
     
-    float getDistance(){
-        return sqrtf((this->x * this->x) * (this->y * this->y));
+    float getLength(){
+        return std::sqrt((this->x * this->x) * (this->y * this->y));
+    }
+    float getLengthSquare(){
+        return ((this->x * this->x) * (this->y * this->y));
     }
     
+    void zero(){
+        x = 0.0;
+        y = 0.0;
+    }
+    bool isZero(){
+        bool xBool = std::abs(x - 0.0) < 0.09;
+        bool yBool = std::abs(y - 0.0) < 0.09;
+        return (xBool && yBool);
+    }
+    float getDistance(const Vector2D& vector2D){
+        return std::sqrt((x - vector2D.x)*(x - vector2D.x) * (y - vector2D.y)*(y - vector2D.y));
+    }
+    float getDistanceSquare(const Vector2D& vector2D){
+        return ((x - vector2D.x)*(x - vector2D.x) * (y - vector2D.y)*(y - vector2D.y));
+    }
 };
 
 #endif /* Vector2D_hpp */
